19dad5526bddf5aa5cadc9abd6c45f7408429f1a
reina-duf <reina.dufrene@gmail.com>
2023-11-09 18:02:49 -0700

    updates buttons to be more rounded & breathing space

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('js is loaded');
+/*----- constants -----*/
+const characters = [
+   {name: "nemo", href: "" }, //0
+   {name: "simba", href: "" }, //1
+   {name: "mickey", href: "" }, //2
+   {name: "remy", href: "" }, //3
+   {name: "walle", href: "" }, //4
+   {name: "moana", href: "" }, //5
+   {name: "kevin", href: "" }, //6
+   {name: "mike", href: "" }, //7
+   {name: "sully", href: "" }, //8
+   {name: "woody", href: "" }, //9
+   {name: "buzz", href: "" }, //10
+   {name: "eve", href: "" } //11   
+];
+
+/*----- state variables -----*/
+let table; //array that stores the shuffled card order
+let winner; //true or false
+let numMatches; //need 12 matches to win the game
+let firstCardSel; //set by the player - first card chosen
+let secondCardSel; //set by the player - second card chosen
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
